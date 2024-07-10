fb081dffb01e708bad61e50324bada6e6eee2d12
sezid <bradsajid@gmail.com>
2024-07-10 22:26:06 +0600

    adding-minus buttons and delete button

--- frontend/src/assets/assets.js
+++ frontend/src/assets/assets.js
@@ -60,6 +60,7 @@ import bag_icon from './bag_icon.png'
 import logout_icon from './logout_icon.png'
 import parcel_icon from './parcel_icon.png'
 import dish from './dish.png'
+import trash from './bin.png'
 
 export const assets = {
     logo,
@@ -81,7 +82,8 @@ export const assets = {
     logout_icon,
     bag_icon,
     parcel_icon,
-    dish
+    dish,
+    trash
 }
 
 export const menu_list = [
