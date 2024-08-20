5b25856d479d7226db64b68398e6aab134b93d8d
sezid <bradsajid@gmail.com>
2024-08-20 15:35:41 +0600

    fixing hosting problems

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,12 +3,5 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],build: {
-    outDir: 'dist', // Ensure this matches your Netlify deployment folder
-    rollupOptions: {
-      output: {
-        manualChunks: undefined,
-      },
-    },
-  },
+  plugins: [react()],
 })
